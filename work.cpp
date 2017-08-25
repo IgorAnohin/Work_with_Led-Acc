864f3b77365d8ade48e325c7e70cdecd3b2f8f42
Igor Anohin <igor.anohin@emlid.com>
2017-08-25 18:01:27 +0300

    work:add pointer

--- work.cpp
+++ work.cpp
@@ -31,50 +31,49 @@ enum {
 };
 
 
-void set_color(int color)
+void set_color(PCA9685 *pwm,int color)
 {
-    PCA9685 pwm;
     switch( color )
     {
         case Yellow:
-            pwm.setPWM(2, 0);
-            pwm.setPWM(1, 0);
-            pwm.setPWM(0, 4095);
+            pwm->setPWM(2, 0);
+            pwm->setPWM(1, 0);
+            pwm->setPWM(0, 4095);
             printf("LED is Yellow\n");
         break;
 
         case Cyan:
-            pwm.setPWM(2, 4095);
-            pwm.setPWM(1, 0);
-            pwm.setPWM(0, 0);
+            pwm->setPWM(2, 4095);
+            pwm->setPWM(1, 0);
+            pwm->setPWM(0, 0);
             printf("LED is Cyan\n");
         break;
 
         case Magneta:
-            pwm.setPWM(2, 0);
-            pwm.setPWM(1, 4095);
-            pwm.setPWM(0, 0);
+            pwm->setPWM(2, 0);
+            pwm->setPWM(1, 4095);
+            pwm->setPWM(0, 0);
             printf("LED is Magneta\n");
         break;
 
         case Blue:
-            pwm.setPWM(2, 4095);
-            pwm.setPWM(1, 4095);
-            pwm.setPWM(0, 0);
+            pwm->setPWM(2, 4095);
+            pwm->setPWM(1, 4095);
+            pwm->setPWM(0, 0);
             printf("LED is Blue\n");
         break;
 
         case Red:
-            pwm.setPWM(2, 0);
-            pwm.setPWM(1, 4095);
-            pwm.setPWM(0, 4095);
+            pwm->setPWM(2, 0);
+            pwm->setPWM(1, 4095);
+            pwm->setPWM(0, 4095);
             printf("LED is Red\n");
         break;
 
         case Green:
-            pwm.setPWM(2, 4095);
-            pwm.setPWM(1, 0);
-            pwm.setPWM(0, 4095);
+            pwm->setPWM(2, 4095);
+            pwm->setPWM(1, 0);
+            pwm->setPWM(0, 4095);
             printf("LED is Green\n");
         break;
 
@@ -85,12 +84,12 @@ void set_color(int color)
 }
 
 
-void get_acc(MPU9250 imu,float *ax, float *ay, float *az)
+void get_acc(MPU9250 *imu,float *ax, float *ay, float *az)
 {
-    imu.read_acc();
-    *ax = imu.accelerometer_data[0];
-    *ay = imu.accelerometer_data[1];
-    *az = imu.accelerometer_data[2];
+    imu->read_acc();
+    *ax = imu->accelerometer_data[0];
+    *ay = imu->accelerometer_data[1];
+    *az = imu->accelerometer_data[2];
 }
 
 
@@ -121,13 +120,16 @@ int main()
         return 1;
     }
 
-    PCA9685 pwm;
 
     if (check_apm()) {
         return 1;
     }
 
     //-------------------------------------------------------------------------
+
+        PCA9685 pwm;
+        pwm.initialize();
+
         MPU9250 imu;
         imu.initialize();
 
@@ -136,34 +138,31 @@ int main()
 
     //-------------------------------------------------------------------------
 
-
-    pwm.initialize();
-
     while (true) {
 
-        get_acc(imu,&ax, &ay, &az);
+        get_acc(&imu,&ax, &ay, &az);
 
         X = Get_deviation(ax);
         Y = Get_deviation(ay);
         Z = Get_deviation(az);
 
         if (X == 0 && Y == 0 && Z == 1)            //Neutral position
-            set_color(Yellow);
+            set_color(&pwm, Yellow);
 
         if (X == 1 && Y == 0 && Z == 0)            //Left 90*
-            set_color(Cyan);
+            set_color(&pwm, Cyan);
 
         if (X == -1 && Y == 0 && Z == 0)           //Right 90*
-            set_color(Magneta);
+            set_color(&pwm, Magneta);
 
         if (X == 0 && Y == 1 && Z == 0)            //Back 90*
-            set_color(Blue);
+            set_color(&pwm, Blue);
 
         if (X == 0 && Y == -1 && Z == 0)           //Forward 90*
-            set_color(Red);
+            set_color(&pwm, Red);
 
-        if (X == 0 && Y == 0 && Z == -1)           //invert 180*
-            set_color(Green);
+        if (X == 0 && Y == 0 && Z == -1)           //Invert 180*
+            set_color(&pwm, Green);
 
         sleep(1);
     }
